ce4545c0e352d1b30e635ee82cae84a5aac41658
Rahul Singh <rahulsinghchand28@gmail.com>
2026-08-18 13:08:44 +0530

    Update home page sections - Awards, Hero & Stats

--- frontend/src/landing_Page/home/Awards.js
+++ frontend/src/landing_Page/home/Awards.js
@@ -2,8 +2,53 @@ import React from 'react';
 
 function Awards() {
     return (
-        <div>
-            <h1>Awards</h1>
+        <div className='container p-5 mb-5'>
+            <div className='row'>
+                <div className='col-6 p-5'>
+
+                    <img src='media/images/largestBroker.svg' alt='Awards' />
+                </div>
+
+                <div className='col-6 p-5 mt-5'>
+                    <h1>Largest stock broker in India</h1>
+                    <p className='mb-5'>Millions of investors trust InvestVault to trade and invest
+                        across a wide range of financial products, including:</p>
+
+                    <div className='row'>
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Future and Options</p>
+                                </li>
+                                <li>
+                                    <p>Commodity Derivatives</p>
+                                </li>
+                                <li>
+                                    <p>Currency Derivatives</p>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Stockes & IPOs</p>
+                                </li>
+                                <li>
+                                    <p>Direct Mutual Funds</p>
+                                </li>
+                                <li>
+                                    <p>Bonds & Government Securities</p>
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 }
--- frontend/src/landing_Page/home/Hero.js
+++ frontend/src/landing_Page/home/Hero.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div>
-            <h1>Hero</h1>
+        <div className='container p-5'>
+            <div className='row text-center'>
+                <img src='media/images/homeHero.png' alt='home hero' className='mb-5' />
+                <h1 className='mt-5'> Invest in everything </h1>
+                <p>Online platform to invest in stockes, mututal funds and many more</p>
+                <button className='p-3 btn btn-primary fs-5 mb-5' style={{ width: "20%", margin: "0 auto" }}>Sign Up</button>
+            </div>
         </div>
     );
 }
