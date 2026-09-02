f9398c98e1b5449a67f782016dd13209383d83fa
Rahul Singh <rahulsinghchand28@gmail.com>
2026-09-02 23:42:41 +0530

    Updated to Hero and added props to LeftSection

--- frontend/src/landing_Page/products/Hero.js
+++ frontend/src/landing_Page/products/Hero.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 function Hero() {
     return (
-        <div>
-            <h1>Hero Section</h1>
+        <div className='container'>
+
+            <div className='"text-center mt-5'>
+                <h1>Technology</h1>
+                <h3 className='text-muted mt-3 fs-4'>Sleek, modern and intuitive trading platforms</h3>
+                <p className='mt-3'>Check out our <a href='' style={{ textDecoration: "name" }}>investment offereings <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+            </div>
         </div>
+
     );
 }
 
--- frontend/src/landing_Page/products/LeftSection.js
+++ frontend/src/landing_Page/products/LeftSection.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 
-function LeftSection() {
+function LeftSection({
+    imageURL,
+    productName,
+    productDescription,
+    tryDemo,
+    learnMore,
+    googlePlay,
+    appStore }) {
     return (
-        <div>
-            <h1>Left Section</h1>
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 p-3'>
+                    <img src={imageURL} />
+                </div>
+
+                <div className='col-6'>
+                    <h1>{productName}</h1>
+                    <p>{productDescription}</p>
+                    <a href={tryDemo}>Try Demo</a>
+                    <a href={learnMore}>Learn More</a>
+                    <a href={googlePlay}><img src="media/images/googlePlayBadge.svg" /></a>
+                    <a href={appStore}><img src="media/images/appStoreBadge.svg" /></a>
+                </div>
+
+
+            </div>
+
         </div>
     );
 }
